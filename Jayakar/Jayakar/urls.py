9671dc60ab1824dbebe2c3038ff32af8c59c0111
Addepalli Jayakar <addepallijayakar@gmail.com>
2024-06-10 11:33:32 +0530

    second change

--- Jayakar/Jayakar/urls.py
+++ Jayakar/Jayakar/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('APP/', include('APP.urls')),
+    path('', include('APP.urls')),
 ]
 
 if settings.DEBUG:
