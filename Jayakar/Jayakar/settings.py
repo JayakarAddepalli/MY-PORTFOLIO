71f5e109525c2edfdf5cdeb9f68bce3550bc4ea1
Addepalli Jayakar <addepallijayakar@gmail.com>
2024-06-11 11:42:10 +0530

    changes1

--- Jayakar/Jayakar/settings.py
+++ Jayakar/Jayakar/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
