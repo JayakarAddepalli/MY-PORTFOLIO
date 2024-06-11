b559eefaacada9f05e58959a9496c85159f21009
Addepalli Jayakar <addepallijayakar@gmail.com>
2024-06-11 11:06:48 +0530

    new changes

--- Jayakar/Jayakar/settings.py
+++ Jayakar/Jayakar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!de)q_#o2l5on7x$v1p*#n+xzvg+^k0uz8c1r-)(e%znm^t9-&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
