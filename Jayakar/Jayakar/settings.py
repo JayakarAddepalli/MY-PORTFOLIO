415608f2076648b9b5f1430b0596b20325870a7f
JayakarAddepalli <addepallijayakar@gmail.com>
2024-06-22 21:22:53 +0530

    'changes'

--- Jayakar/Jayakar/settings.py
+++ Jayakar/Jayakar/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +31,7 @@ SECRET_KEY = 'django-insecure-!de)q_#o2l5on7x$v1p*#n+xzvg+^k0uz8c1r-)(e%znm^t9-&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [STATIC_DIR]
+django_heroku.settings(locals())
 
 MEDIA_URL = 'media/'
 
